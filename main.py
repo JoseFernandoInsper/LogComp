677f764826f4d04fbf62d4d3bc0295f862f54e17
JoseFernandoInsper <josefmc@al.insper.edu.br>
2021-09-14 14:42:41 -0300

    roteiro 2

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import sys
 
 lg = LexerGenerator()
 
-lg.add('NUMBER', r'\d+')
+lg.add('NUMBER', r'[+-]?\d+')
 lg.add('PLUS', r'\+')
 lg.add('MINUS', r'-')
 lg.add('MUL', r'\*')
 lg.add('DIV', r'/')
+lg.add('OPEN_PARENS', r'\(')
+lg.add('CLOSE_PARENS', r'\)')
 
 lg.ignore(r'\/\*(.*?)\*\/')
 lg.ignore('\s+')
@@ -48,7 +50,7 @@ class Div(BinaryOp):
 
 pg = ParserGenerator(
     # A list of all token names, accepted by the parser.
-    ['NUMBER', 'PLUS', 'MINUS', 'MUL', 'DIV'
+    ['NUMBER','OPEN_PARENS', 'CLOSE_PARENS', 'PLUS', 'MINUS', 'MUL', 'DIV'
     ],
     # A list of precedence rules with ascending precedence, to
     # disambiguate ambiguous production rules.
@@ -63,6 +65,10 @@ def expression_number(p):
     # p is a list of the pieces matched by the right hand side of the
     # rule
     return Number(int(p[0].getstr()))
+
+@pg.production('expression : OPEN_PARENS expression CLOSE_PARENS')
+def expression_parens(p):
+    return p[1]
 
 @pg.production('expression : expression PLUS expression')
 @pg.production('expression : expression MINUS expression')
