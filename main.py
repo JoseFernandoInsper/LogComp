1579e47dc27aa21587a4f3acf6af71feb39e49ef
JoseFernandoInsper <josefmc@al.insper.edu.br>
2021-12-14 11:21:13 -0300

    if while quebrado

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 
 #LEXER
 lg = LexerGenerator()
+#lg.add('INT', r'int')
+#lg.add('DOUBLE', r'double')
 lg.add('NUMBER', r'\d+')
 lg.add('PLUS', r'\+')
 lg.add('MINUS', r'-')
@@ -12,12 +14,25 @@ lg.add('MUL', r'\*')
 lg.add('DIV', r'/')
 lg.add('OPEN_PARENS', r'\(')
 lg.add('CLOSE_PARENS', r'\)')
+lg.add('OPEN_CHAVE', r'{')
+lg.add('CLOSE_CHAVE', r'}')
 lg.add('PRINT', r'println')
 lg.add('EQUAL', r'=')
-lg.add('SEMI', r'\;')
+lg.add('SEMI', r';')
 lg.add('IDENTIFIER', r'[a-zA-Z_]([a-zA-Z_0-9]*|_[a-zA-Z_0-9]*)')
-#lg.add('IF', 'if')
-#lg.add('WHILE', r'while')
+lg.add('IF', r'if')
+lg.add("ELSE", r'else')
+#lg.add('FOR', r'for')
+lg.add('WHILE', r'while')
+lg.add('EQUIVALENT', r'\=\=')
+lg.add('DIFF', r'\!=')
+lg.add('GET', r'\>=')
+lg.add('LET', r'\<=')
+lg.add('GT', r'\>')
+lg.add('LT', r'\<')
+lg.add('OR', r'\|\|')
+lg.add('AND', r'\&\&')
+lg.add('NOT', r'\!')
 
 
 lg.ignore(r'\/\*(.*?)\*\/')
@@ -52,6 +67,8 @@ class UnOp(Node):
             return self.children[0].eval()
         elif self.value == 'NEGATIVE':
             return -(self.children[0].eval())
+        elif self.value == 'NOT':
+            return not(self.children[0].eval())
 
 class BinOp(Node):
     def __init__(self, left, right):
@@ -73,12 +90,63 @@ class Div(BinOp):
     def eval(self):
         return int(self.children[0].eval() / self.children[1].eval())
 
+class And(BinOp):
+    def eval(self):
+        return self.children[0].eval() and self.children[1].eval()
+
+class Or(BinOp):
+    def eval(self):
+        return self.children[0].eval() or self.children[1].eval()
+
+class Equivalent(BinOp):
+    def eval(self):
+        return self.children[0].eval() == self.children[1].eval()
+
+class Diff(BinOp):
+    def eval(self):
+        return self.children[0].eval() != self.children[1].eval()
+
+class GET(BinOp):
+    def eval(self):
+        return self.children[0].eval() >= self.children[1].eval()
+
+class LET(BinOp):
+    def eval(self):
+        return self.children[0].eval() <= self.children[1].eval()
+
+class GT(BinOp):
+    def eval(self):
+        return self.children[0].eval() > self.children[1].eval()
+
+class LT(BinOp):
+    def eval(self):
+        return self.children[0].eval() < self.children[1].eval()
+
 class Print(Node):
     def __init__(self, value):
         self.value = value
 
     def eval(self):
         return self.value.eval()
+
+class While():
+    def __init__(self, value):
+        self.value = value
+    
+    def eval(self):
+        while self.value[0].eval():
+            self.value[1].eval()
+
+class IfElse():
+    def __init__(self, value):
+        self.value = value
+    
+    def eval(self):
+        if self.value[0].eval():
+            self.value[1].eval()
+        else:
+            if self.value[2] != None:
+                self.value[2].eval()
 
 class SymbolTable:
     def __init__(self):
@@ -122,17 +190,26 @@ class Program():
                 if(i.eval() != None):
                     print(i.eval())
 
+#'INT', 'DOUBLE', 'FOR',
 pg = ParserGenerator(
     # A list of all token names, accepted by the parser.
-    ['NUMBER','OPEN_PARENS', 'CLOSE_PARENS', 'PLUS', 'MINUS', 'MUL', 'DIV', 'PRINT', 'EQUAL', 'SEMI', 'IDENTIFIER'
+    ['NUMBER', 'OPEN_PARENS', 'CLOSE_PARENS', 'OPEN_CHAVE', 'CLOSE_CHAVE','PLUS', 'MINUS', 'MUL', 'DIV', 'PRINT', 'EQUAL', 'SEMI', 'IDENTIFIER',
+    'IF', 'ELSE', 'WHILE', 'EQUIVALENT', 'DIFF', 'GET', 'LET', 'GT', 'LT', 'OR', 'AND', 'NOT'
     ],
     # A list of precedence rules with ascending precedence, to
     # disambiguate ambiguous production rules.
     precedence=[
         ('left', ['PLUS', 'MINUS']),
-        ('left', ['MUL', 'DIV'])
+        ('left', ['MUL', 'DIV']),
+        ('left', ['NOT']),
+        ('left', ['OR', 'AND', 'EQUIVALENT', 'DIFF', 'GET', 'LET'] ),
+        ('left', ['GT', 'LT'])
     ]
 )
+
+@pg.production('input : OPEN_CHAVE program CLOSE_CHAVE ')
+def input(p):
+    return p[1]
 
 @pg.production('program : statement')
 @pg.production('program : program statement')
@@ -143,6 +220,8 @@ def prog_state(p):
     return p[0]
 
 @pg.production('statement : println')
+@pg.production('statement : while')
+@pg.production('statement : if')
 def statement(p):
     return p[0]
 
@@ -154,21 +233,40 @@ def statement(p):
     else:
         return None
 
-
 @pg.production('println : PRINT OPEN_PARENS expression CLOSE_PARENS SEMI')
 @pg.production('println : PRINT OPEN_PARENS variable CLOSE_PARENS SEMI')
 def println(p):
     return Print(p[2])
 
+@pg.production('if : IF OPEN_PARENS expression CLOSE_PARENS OPEN_CHAVE input CLOSE_CHAVE ELSE OPEN_CHAVE input CLOSE_CHAVE')
+@pg.production('if : IF OPEN_PARENS expression CLOSE_PARENS OPEN_CHAVE input CLOSE_CHAVE')
+def IfFunc(p):
+    condElm = p[2]
+    iftrue = p[5]
+    if len(p) > 7:
+        elsetrue = p[9]
+        return IfElse([condElm, iftrue, elsetrue])
+    else:
+        return IfElse([condElm, iftrue, None])
+
+
+@pg.production('while : WHILE OPEN_PARENS expression CLOSE_PARENS OPEN_CHAVE input CLOSE_CHAVE')
+def WhileFunc(p):
+    condElm = p[2]
+    loop = p[4]
+    return While([condElm, loop])
+
 @pg.production('expression : PLUS expression')
 @pg.production('expression : MINUS expression')
+@pg.production('expression : NOT expression')
 def expression_unary(p):
     unario = p[0].gettokentype()
     if (unario == 'PLUS'):
         return UnOp('POSITIVE', p[1])
     elif  (unario == 'MINUS'):
         return UnOp('NEGATIVE', p[1])
-
+    elif (unario == 'NOT'):
+        return UnOp('NOT', p[1])
 
 @pg.production('expression : NUMBER')
 def expression_number(p):
@@ -201,6 +299,15 @@ def variable(p):
 @pg.production('expression : expression MINUS expression')
 @pg.production('expression : expression MUL expression')
 @pg.production('expression : expression DIV expression')
+###
+@pg.production('expression : expression AND expression')
+@pg.production('expression : expression OR expression')
+@pg.production('expression : expression EQUIVALENT expression')
+@pg.production('expression : expression DIFF expression')
+@pg.production('expression : expression GET expression')
+@pg.production('expression : expression LET expression')
+@pg.production('expression : expression GT expression')
+@pg.production('expression : expression LT expression')
 
 
 def expression_binop(p):
@@ -214,6 +321,22 @@ def expression_binop(p):
         return Mul(left, right)
     elif p[1].gettokentype() == 'DIV':
         return Div(left, right)
+    elif p[1].gettokentype() == 'AND':
+        return And(left, right)
+    elif p[1].gettokentype() == 'OR':
+        return Or(left, right)
+    elif p[1].gettokentype() == 'EQUIVALENT':
+        return Equivalent(left, right)
+    elif p[1].gettokentype() == 'DIFF':
+        return Diff(left, right)
+    elif p[1].gettokentype() == 'GET':
+        return GET(left, right)
+    elif p[1].gettokentype() == 'LET':
+        return LET(left, right)
+    elif p[1].gettokentype() == 'GT':
+        return GT(left, right)
+    elif p[1].gettokentype() == 'LT':
+        return LT(left, right)
     else:
         raise AssertionError('Oops, this should not be possible!')
 
